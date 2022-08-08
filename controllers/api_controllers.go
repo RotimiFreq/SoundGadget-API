e2f575102dcf2c056bfaee0c58632e64fb39aa9c
RotimiFreq <oluwarotimihz@gmail.com>
2022-08-08 23:28:30 +0100

    I had two more endpoints

--- controllers/api_controllers.go
+++ controllers/api_controllers.go
@@ -1,57 +1,112 @@
 package controllers
 
 import (
+	"SoundGadget-API/configs"
+	"SoundGadget-API/models"
+	"SoundGadget-API/responses"
+	"context"
+	"net/http"
+	"time"
 
-    "context"
-    "SoundGadget-API/configs"
-    "SoundGadget-API/models"
-    "SoundGadget-API/responses"
-    "net/http"
-    "time"
-  
-    "github.com/go-playground/validator/v10"
-    "github.com/gofiber/fiber/v2"
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
-
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-
-
-var ApiCollection *mongo.Collection = configs.GetCollection(configs.DB, "Mixers")
+// validator variable to validate our string
 
 var validate = validator.New()
 
+// variable for our data models
+var product models.Product
 
-func CreateMixer(c *fiber.Ctx) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	var mixer models.Mixers
+// function create a new collection in out database
+func createColl(collectionName string) *mongo.Collection {
+
+	createdColl := configs.GetCollection(configs.DB, collectionName)
+
+	return createdColl
+
+}
+
+var newColl = createColl("Mixers")
+
+func CreateProduct(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 
 	defer cancel()
 
-	if err := c.BodyParser(&mixer); err != nil{
+	if err := c.BodyParser(&product); err != nil {
 		return c.Status(http.StatusBadRequest).JSON(responses.ApiResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
 
 	}
 
-
-	if validErr := validate.Struct(&mixer); validErr != nil {
+	if validErr := validate.Struct(&product); validErr != nil {
 		return c.Status(http.StatusBadRequest).JSON(responses.ApiResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validErr.Error()}})
 	}
 
-	newMixer := models.Mixers{
-		Id : primitive.NewObjectID(),
-		Brand: "Allen&Heath",
-		Name: "QU-32",
-		Price: 2350,
-		MixerDetails: "This is a user friendly console. it has 32 input and 16 outputs",
+	// data body for the products
+
+	newMixer := models.Product{
+		Id:           primitive.NewObjectID(),
+		ProductType:  product.ProductType,
+		Brand:        product.Brand,
+		Name:         product.Name,
+		Price:        product.Price,
+		MixerDetails: product.MixerDetails,
 	}
 
-	result, errInsert := ApiCollection.InsertOne(ctx, newMixer)
+	newMic := models.Product{
+		Id:            primitive.NewObjectID(),
+		ProductType:   product.ProductType,
+		Brand:         product.Brand,
+		Name:          product.Name,
+		Price:         product.Price,
+		PickupPattern: product.PickupPattern,
+		MicType:       product.MicType,
+		Wireless:      product.Wireless,
+	}
+	newProcessor := models.Product{
+		Id:               primitive.NewObjectID(),
+		ProductType:      product.ProductType,
+		Brand:            product.Brand,
+		Name:             product.Name,
+		Price:            product.Price,
+		ProcessorType:    product.ProcessorDetails,
+		ProcessorDetails: product.ProcessorDetails,
+	}
+
+	newSpeaker := models.Product{
+		Id:                  primitive.NewObjectID(),
+		ProductType:         product.ProductType,
+		Brand:               product.Brand,
+		Name:                product.Name,
+		Price:               product.Price,
+		SpeakerPeakpower:    product.SpeakerPeakpower,
+		SpeakerProgrampower: product.SpeakerProgrampower,
+		SpeakerType:         product.SpeakerType,
+		SpeakerAbout:        product.SpeakerAbout,
+	}
+
+	newAmp := models.Product{
+		Id:                   primitive.NewObjectID(),
+		ProductType:          product.ProductType,
+		Brand:                product.Brand,
+		Name:                 product.Name,
+		Price:                product.Price,
+		AmplifierType:        product.AmplifierType,
+		AmplifierPowerRating: product.AmplifierPowerRating,
+		AmplifierAbout:       product.AmplifierAbout,
+	}
+
+	//inserting the data body into the database
+
+	result, errInsert := newColl.InsertOne(ctx, newMixer)
 
 	if errInsert != nil {
-		return c.Status(http.StatusInternalServerError).JSON(responses.ApiResponse{Status: http.StatusInternalServerError, Message:"error", Data: &fiber.Map{"data": errInsert.Error()}})
+		return c.Status(http.StatusInternalServerError).JSON(responses.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": errInsert.Error()}})
 	}
 
 	return c.Status(http.StatusCreated).JSON(responses.ApiResponse{Status: http.StatusCreated, Message: "success", Data: &fiber.Map{"data": result}})
@@ -62,21 +117,20 @@ func CreateMixer(c *fiber.Ctx) error {
 
 func GetMixer(c *fiber.Ctx) error {
 
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	Id := c.Params("Id")
 
-    Id := c.Params("Id")
+	var themixer models.Product
 
-    var themixer models.Mixers
+	defer cancel()
 
-    defer cancel()
+	objId, _ := primitive.ObjectIDFromHex(Id)
 
-    objId, _ := primitive.ObjectIDFromHex(Id)
+	errFind := newColl.FindOne(ctx, bson.M{"id": objId}).Decode(&themixer)
+	if errFind != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": errFind.Error()}})
+	}
 
-    errFind := ApiCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&themixer)
-    if errFind != nil {
-        return c.Status(http.StatusInternalServerError).JSON(responses.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": errFind.Error()}})
-    }
-
-    return c.Status(http.StatusOK).JSON(responses.ApiResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": themixer}})
+	return c.Status(http.StatusOK).JSON(responses.ApiResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": themixer}})
 }
